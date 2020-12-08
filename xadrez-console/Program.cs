54c31dd7a11982c475ead3836b5c072934db62fa
Pedro HSA <phenriquesa94@gmail.com>
2020-12-08 09:51:02 -0300

    Projeto Inicial

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
