35c595322ada109d2a16488820ff6c7001726db2
Pedro <pedro.si.dev@gmail.com>
2021-01-04 10:18:59 -0300

    Colocando pecas no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -21,5 +21,11 @@ namespace tabuleiro
         {
             return pecas[linha, coluna];
         }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,17 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
